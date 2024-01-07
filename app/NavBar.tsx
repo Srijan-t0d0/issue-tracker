65c4bb519613342e86a800086eef238d10f8d8e1
Srijan <chaturchacharost@gmail.com>
2024-01-07 10:20:32 +0530

    setting up prisma and creating issue model

---
+++ app/NavBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const Currentpath = usePathname();
+  const navLinks = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-4 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {navLinks.map((link) => (
+          <li>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === Currentpath,
+                "text-zinc-500": link.href !== Currentpath,
+                "hover:text-zinc-900 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
